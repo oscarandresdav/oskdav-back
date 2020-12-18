f610f2a8695834464cd1dd49c045edea7815374c
Oscar Davila <oscar7822002@gmail.com>
2020-12-17 22:51:14 -0500

    Uso de parametros de ruta

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('brands')
 export class BrandsController {
   @Get()
   findAll() {
     return 'This action returns all brands';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action return #${id} identification`;
   }
 }
